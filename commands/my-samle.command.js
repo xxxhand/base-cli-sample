733a904c8d207a0e91eb0982cb5244e9ab56cf74
xxxhand <nohandboy@gmail.com>
2025-05-02 07:36:32 +0800

    add formater

--- commands/my-samle.command.js
+++ commands/my-samle.command.js
@@ -13,28 +13,28 @@ const _CMD_NAME = 'my';
 
 /**
  * @description Command handler
- * @param {'print'|'smile'} action 
+ * @param {'print'|'smile'} action
  * @param {IMyOption} opt
  * @returns {Promise<void>}
  */
 async function cmdHanlder(action, opt) {
-    new CustomValidator()
-        .checkThrows(action, { m: `Action ${action} not accepted.`, fn: (val) => _ACCEPT_ACTIONS.includes(val) })
-        .checkThrows(opt.name, { s: CustomDefinition.validateStrategy.NON_EMPTY_STRING, m: 'You must input a name' })
-        .checkThrows(opt.date, { s: CustomDefinition.validateStrategy.NON_EMPTY_STRING, m: 'You must input a date' })
-    
-    if (action === 'print') {
-        console.log(`${getConf().hello}, ${opt.name} call the print on ${opt.date}`);
-    } else {
-        console.log(`OOps, ${opt.name} is ${action} now!!!`);
-    }
+  new CustomValidator()
+    .checkThrows(action, { m: `Action ${action} not accepted.`, fn: (val) => _ACCEPT_ACTIONS.includes(val) })
+    .checkThrows(opt.name, { s: CustomDefinition.validateStrategy.NON_EMPTY_STRING, m: 'You must input a name' })
+    .checkThrows(opt.date, { s: CustomDefinition.validateStrategy.NON_EMPTY_STRING, m: 'You must input a date' });
+
+  if (action === 'print') {
+    console.log(`${getConf().hello}, ${opt.name} call the print on ${opt.date}`);
+  } else {
+    console.log(`OOps, ${opt.name} is ${action} now!!!`);
+  }
 }
 
 // Initial command
 /** @type {import('commander').Command} */
 export const mySampleCommand = new Command(_CMD_NAME)
-    .argument('[action]', `Action to perform, accepted values: ${_ACCEPT_ACTIONS.join(', ')}`)
-    .addOption(new Option('-n, --name [string]', 'Optional: Your name.').default('xxxhand'))
-    .addOption(new Option('-d, --date [string]', 'Optional: Date in yyyy-mm-dd').default(new Date().toISOString().substring(0, 10)))
-    .description('My first cli command')
-    .action(cmdHanlder);
+  .argument('[action]', `Action to perform, accepted values: ${_ACCEPT_ACTIONS.join(', ')}`)
+  .addOption(new Option('-n, --name [string]', 'Optional: Your name.').default('xxxhand'))
+  .addOption(new Option('-d, --date [string]', 'Optional: Date in yyyy-mm-dd').default(new Date().toISOString().substring(0, 10)))
+  .description('My first cli command')
+  .action(cmdHanlder);
